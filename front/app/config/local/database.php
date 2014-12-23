791eb1df682c0ca69932811c40083468f74c0b12
masuhajime <masuhajimep@gmail.com>
2014-12-23 17:56:10 +0000

    mysql setting

--- front/app/config/local/database.php
+++ front/app/config/local/database.php
@@ -20,27 +20,19 @@ return array(
 
 	'connections' => array(
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'mer',
+            'username'  => 'root',
+            'password'  => '12345678',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'options'   => array(
+                PDO::ATTR_PERSISTENT => true,
+            ),
+        ),
 
 	),
 
